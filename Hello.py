12857a1eb9cce45900bf3ce2c690648c788bb14e
Eric Strong <c15708709@mydit.ie>
2019-12-01 15:22:55 +0000

    added something new

--- Hello.py
+++ Hello.py
@@ -5,7 +5,7 @@ def printMany(text):
 def main():
     print('Hello!')
     for i in range(10):
-        printMany("Eric Strong")
+        printMany("Eric Strong and Laura")
 
 if __name__ == "__main__":
     main()
